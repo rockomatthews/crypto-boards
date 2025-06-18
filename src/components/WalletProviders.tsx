7bf5986d6246c23a009b32c507ed550489e06176
rockomatthews <rob@robmatthe.ws>
2025-06-18 16:50:52 -0600

    fix vercel build issue

--- src/components/WalletProviders.tsx
+++ src/components/WalletProviders.tsx
@@ -3,7 +3,8 @@
 import { FC, ReactNode, useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
+import { SolflareWalletAdapter } from '@solana/wallet-adapter-solflare';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 
@@ -21,12 +22,12 @@ export const WalletProviders: FC<Props> = ({ children }) => {
   // You can also provide a custom RPC endpoint
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
-  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading
+  // Initialize wallet adapters
   const wallets = useMemo(
     () => [
       new PhantomWalletAdapter(),
       new SolflareWalletAdapter(),
-    ] as unknown as (PhantomWalletAdapter | SolflareWalletAdapter)[],
+    ],
     []
   );
 
